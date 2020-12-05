e58a7216d77051ba57037e90f9876ba4bd7ab7e8
bosko2004 <marko.karlovac41@gmail.com>
2020-12-05 17:10:14 +0000

    project structure easy node creation program is almost done(needed testing) started environnment

---
+++ go.mod
@@ -0,0 +1,3 @@
+module notabug.org/bosko/erigami
+
+go 1.15
---
+++ pkg/generate/gen.go
@@ -0,0 +1,8 @@
+package generate
+
+import "notabug.org/bosko/erigami/pkg/node"
+
+//code generation :TODO
+func Generate(ns []node.Node) []node.Node {
+	return ns
+}
